a38a438cc4c0aae1b4d93c8806ec838cb877e5ec
Duke <juan.carlos@cceo.com.mx>
2019-03-22 16:06:25 -0600

    create a dynamic table with the help of v-on and methods

--- js/principal.js
+++ js/principal.js
@@ -2,9 +2,23 @@ var app=new Vue({
     el :'#app',
     data:{
         lista : [
-            {name:'Juan Carlos',average:'10',status: false},
-            {name:'Liliana Nataly',average:'17',status: true},
-            {name:'Luis Alberto',average:'18',status: true}
+            
         ],
-    }
+    },
+    methods: {
+       clicknote: function(){
+            statusA=false;
+            if(this.average>=12.5){
+                statusA=true;
+            }
+            if(this.name!==""&& this.average!=""){
+                this.lista.push({name:this.name,average:this.average,status:statusA});
+                this.name="";
+                this.average="";
+            }
+            else{
+                alert("Ingrese el nombre y promedio del estudiante")
+            }
+        }
+    },
 })
