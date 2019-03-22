26b494db70f8eeb4da07c9c7bfbf030f2aee9b17
JuanjoDuke <48736286+JuanjoDuke@users.noreply.github.com>
2019-03-22 16:17:07 -0600

    Features/methods and directive v on (#3)
    * Create some directives: v-text, v-model
    
    * Main structure of tables
    
    * place 3 records
    
    * create directives: for, bind and put text in English
    
    * change the states
    
    * create a dynamic table with the help of v-on and methods

--- js/principal.js
+++ js/principal.js
@@ -1,6 +1,24 @@
 var app=new Vue({
     el :'#app',
     data:{
-     mensaje:'Hola vue.js!',
-    }
+        lista : [
+            
+        ],
+    },
+    methods: {
+       clicknote: function(){
+            statusA=false;
+            if(this.average>=12.5){
+                statusA=true;
+            }
+            if(this.name!==""&& this.average!=""){
+                this.lista.push({name:this.name,average:this.average,status:statusA});
+                this.name="";
+                this.average="";
+            }
+            else{
+                alert("Ingrese el nombre y promedio del estudiante")
+            }
+        }
+    },
 })
